c0346199b2090ce593cbbac308ac0a6e1312fe60
Abuzar <hamzamushtaq362@gmail.com>
2024-11-01 15:49:33 +0500

    top heading

--- src/components/QuickCheckout/QuickCheckout.jsx
+++ src/components/QuickCheckout/QuickCheckout.jsx
@@ -4,9 +4,14 @@ import OrderForm from "./OrderForm";
 
 export default function QuickCheckout() {
   return (
-    <div className="w-full flex">
-      <RegistrationForm />
-      <OrderForm />
+    <div>
+      <div className="w-full space-y-8 p-4 bg-gray-50 border">
+        <h2 className="text-2xl font-semibold">Quick Checkout</h2>
+      </div>
+      <div className="w-full flex">
+        <RegistrationForm />
+        <OrderForm />
+      </div>
     </div>
   );
 }
